9ce4e339fdabff8453e8a0479b3b46cde87407e6
Renato Lobo <renatolobo_@hotmail.com>
2021-03-12 22:46:46 -0300

    Listando Repositories

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -2,17 +2,11 @@ import {useEffect, useState} from 'react';
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss' ;
 
-const repository = {
-  name: 'Padrão',
-  description: 'Forms in React',
-  link: 'https://github.com/unform/unform'
-}
-
 export function RepositoryList() {
   const [repositories, setRepositories] = useState([]);
 
   useEffect(() => {
-    fetch('https://api.github.com/users/renatolobo1/repos')
+    fetch('https://api.github.com/orgs/rocketseat/repos')
     .then(response => response.json())
     .then(data =>setRepositories(data))
   },[]);
@@ -22,11 +16,10 @@ export function RepositoryList() {
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-
+        {repositories.map(repository =>{
+          return <RepositoryItem key={repository.name} repository={repository}/> 
+        })}
+               
       </ul>
     </section>
   )
